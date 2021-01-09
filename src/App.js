377499ffd9c0de4a827b28921ffaf18380b959a5
Robert Nguyen <robert@gotitapp.co>
2021-01-09 19:15:20 +0700

    feat: adding plugin-porposal-class-properties for babel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
-export default () => {
-  return (
-    <div>
-      This is React Code!!!!
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Count: {this.state.count} </h2>
+        <button 
+          onClick={() => this.setState(state => ({count: state.count + 1}))}
+        >
+          +
+        </button>
+        <button 
+          onClick={() => this.setState(state => ({count: state.count - 1}))}>
+          -
+        </button>
+      </div>
+    )
+  }
 }
 
+export default App
