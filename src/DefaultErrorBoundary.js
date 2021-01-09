4ac9187ce852b909cd1f56cf8db85dc0b864978f
Robert Nguyen <robert@gotitapp.co>
2021-01-09 22:50:07 +0700

    feat: added DefaultErrorBoundary

---
+++ src/DefaultErrorBoundary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class DefaultErrorBoundary extends React.Component {
+  state = {
+    isError: false,
+  }
+
+  static getDerivedStateFromError() {
+    return { isError: true }
+  }
+
+  render() {
+    const { isError } = this.state
+    const { children } = this.props
+    return isError ? <div>Something went wrong!</div> : children
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 import '@babel/polyfill' // utilize polyfill
 import './styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 )
